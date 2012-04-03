2151c230890a75696b0d1ea2cb4729ebc3e52b4d
animal <animal@765c1f7c-9fb8-954f-9ff8-dd0915cb3117>
2012-04-03 02:41:27 +0000

    MPfm 0.6.0.0 - Created and added the MPfm.UI project to the solution.
    Related to issue #324.
    
    
    git-svn-id: http://hamster-svn/svn/repos/base@383 765c1f7c-9fb8-954f-9ff8-dd0915cb3117

---
+++ MPfm/branches/current/MPfm.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MPfm UI Components")]
+[assembly: AssemblyDescription("Contains the MPfm multi-plaform user interface code.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yanick Castonguay")]
+[assembly: AssemblyProduct("MPfm (Music Player for Musicians)")]
+[assembly: AssemblyCopyright("Copyright © 2011-2012 Yanick Castonguay")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e49b26a-3ae5-4ab2-bf63-a72fcf7c9530")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
