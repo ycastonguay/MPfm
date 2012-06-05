f10bab72375f3024dc12010dd5ef082b3bfb771b
animal <animal@765c1f7c-9fb8-954f-9ff8-dd0915cb3117>
2012-06-05 03:58:31 +0000

    Initial import of MPfm iOS project (third take)
    git-svn-id: http://hamster-svn/svn/repos/base@541 765c1f7c-9fb8-954f-9ff8-dd0915cb3117

---
+++ iOS/MPfm/MPfm/MPfmAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPfmAppDelegate.h
+//  MPfm
+//
+//  Created by Yanick Castonguay on 12-06-04.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPfmAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/MPfm/MPfm/MPfmFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPfmFirstViewController.h
+//  MPfm
+//
+//  Created by Yanick Castonguay on 12-06-04.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPfmFirstViewController : UIViewController
+
+@end
