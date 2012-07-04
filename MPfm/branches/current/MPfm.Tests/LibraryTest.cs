d80a6b2e587cd3bf3ee51f0f703d4f96cbd1fbb4
animal <animal@765c1f7c-9fb8-954f-9ff8-dd0915cb3117>
2012-07-04 02:10:53 +0000

    MPfm 0.6.0.1 * Added new MPfm.Tests project. * Mac   - Added reference to MPfm.Tests project in the MPfm_Mac solution file.
    Related to issue #379.
    
    git-svn-id: http://hamster-svn/svn/repos/base@591 765c1f7c-9fb8-954f-9ff8-dd0915cb3117

---
+++ MPfm/branches/current/MPfm.Tests/LibraryTest.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+
+namespace MPfm.Tests
+{
+    [TestFixture()]
+    public class LibraryTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+
+        [Test()]
+        public void TestCaseTwo()
+        {
+        }
+    }
+}
+
