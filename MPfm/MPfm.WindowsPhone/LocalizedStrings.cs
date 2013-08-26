2dd585ac403ff1d90b3afd363dcdb63202a8995b
ycastonguay <ycastonguay@mp4m.org>
2013-08-26 19:26:05 -0400

    WindowsPhone: Added empty project for prototyping.
    Related to issue #424.

---
+++ MPfm/MPfm.WindowsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MPfm.WindowsPhone.Resources;
+
+namespace MPfm.WindowsPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
