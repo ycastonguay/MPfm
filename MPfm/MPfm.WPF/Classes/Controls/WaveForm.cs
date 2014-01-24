464ab8e921f54fa1e91300d26a932a743b3290eb
ycastonguay <ycastonguay@mp4m.org>
2014-01-24 01:54:59 -0500

    Added missing files from last commit.

---
+++ MPfm/MPfm.WPF/Classes/Controls/WaveForm.cs
@@ -0,0 +1,112 @@
+﻿// Copyright © 2011-2013 Yanick Castonguay
+//
+// This file is part of MPfm.
+//
+// MPfm is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MPfm is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MPfm. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Threading;
+using MPfm.GenericControls.Controls;
+using MPfm.Player.Objects;
+using MPfm.Sound.AudioFiles;
+using MPfm.WPF.Classes.Controls.Graphics;
+using Control = System.Windows.Controls.Control;
+
+namespace MPfm.WPF.Classes.Controls
+{
+    public class WaveForm : Control
+    {
+        private readonly WaveFormControl _control;
+
+        public long Position
+        {
+            get
+            {
+                return _control.Position;
+            }
+            set
+            {
+                _control.Position = value;
+            }
+        }
+
+        public long SecondaryPosition
+        {
+            get
+            {
+                return _control.SecondaryPosition;
+            }
+            set
+            {
+                _control.SecondaryPosition = value;
+            }
+        }
+
+        public bool ShowSecondaryPosition
+        {
+            get
+            {
+                return _control.ShowSecondaryPosition;
+            }
+            set
+            {
+                _control.ShowSecondaryPosition = value;
+            }
+        }
+
+        public WaveForm()
+        {
+            _control = new WaveFormControl();
+            _control.OnInvalidateVisual += () => Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(InvalidateVisual));
+            _control.OnInvalidateVisualInRect += (rect) => Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                InvalidateVisual();
+                // TODO: It seems you can't invalidate a specific rect in WPF? What?
+                // http://stackoverflow.com/questions/2576599/possible-to-invalidatevisual-on-a-given-region-instead-of-entire-wpf-control                                                                                                                       
+            }));
+        }
+
+        public void SetMarkers(IEnumerable<Marker> markers)
+        {
+            _control.SetMarkers(markers);
+        }
+
+        public void SetWaveFormLength(long lengthBytes)
+        {
+            _control.Length = lengthBytes;
+        }
+
+        public void LoadPeakFile(AudioFile audioFile)
+        {
+            _control.LoadPeakFile(audioFile);
+        }
+
+        public void RefreshWaveFormBitmap(int width)
+        {
+            _control.RefreshWaveFormBitmap(width);
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            Console.WriteLine("WaveForm - OnRender - width: {0} height: {1}", ActualWidth, ActualHeight);
+            base.OnRender(dc);
+            var wrapper = new GraphicsContextWrapper(dc, (float) ActualWidth, (float) ActualHeight);
+            if (ActualWidth == 0 || ActualHeight == 0)
+                return;
+            //_control.Render(wrapper);
+        }   
+    }
+}
---
+++ MPfm/MPfm.WPF/Classes/Controls/WaveFormScrollViewer.cs
@@ -0,0 +1,71 @@
+﻿// Copyright © 2011-2013 Yanick Castonguay
+//
+// This file is part of MPfm.
+//
+// MPfm is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MPfm is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MPfm. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MPfm.Sound.AudioFiles;
+
+namespace MPfm.WPF.Classes.Controls
+{
+    public class WaveFormScrollViewer : ScrollViewer
+    {
+        public WaveForm WaveFormView { get; private set; }
+        public WaveFormScale WaveFormScaleView { get; private set; }
+
+        public WaveFormScrollViewer()
+        {
+            VerticalScrollBarVisibility = ScrollBarVisibility.Disabled;
+            Background = new SolidColorBrush(Colors.BlueViolet);
+
+            WaveFormView = new WaveForm();
+            WaveFormScaleView = new WaveFormScale();
+
+            //var stackPanel = new StackPanel();
+            //stackPanel.Orientation = Orientation.Horizontal;
+            //stackPanel.Children.Add(WaveFormScaleView);
+            //stackPanel.Children.Add(WaveFormView);
+            //Content = stackPanel;
+
+            var grid = new Grid();
+            var rowScale = new RowDefinition();
+            var rowWaveForm = new RowDefinition();
+            rowScale.Height = new GridLength(22);
+            rowWaveForm.Height = new GridLength(60);
+            grid.RowDefinitions.Add(rowScale);
+            grid.RowDefinitions.Add(rowWaveForm);
+            grid.Children.Add(WaveFormScaleView);
+            grid.Children.Add(WaveFormView);
+            Grid.SetRow(WaveFormScaleView, 0);
+            Grid.SetRow(WaveFormView, 1);
+            Content = grid;
+        }
+
+        public void LoadPeakFile(AudioFile audioFile)
+        {
+            WaveFormView.LoadPeakFile(audioFile);
+            WaveFormScaleView.AudioFile = audioFile;
+        }
+
+        public void SetWaveFormLength(long lengthBytes)
+        {
+            WaveFormView.SetWaveFormLength(lengthBytes);
+            WaveFormScaleView.AudioFileLength = lengthBytes;
+        }
+    }
+}
