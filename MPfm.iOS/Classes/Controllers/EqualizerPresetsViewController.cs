b72c4780bddf0a770eba4696356cfeb61b453399
ycastonguay <ycastonguay@mp4m.org>
2014-05-16 00:54:33 -0400

    iOS: Replaced Dropbox component by an assembly in /lib
    - Removed Dropbox component because it seems that projects with Xamarin
    components cannot be built in Jenkins; replaced the dependency by the
    assembly in /Lib.
    - Updated project after changes on other platforms

--- MPfm.iOS/Classes/Controllers/EqualizerPresetsViewController.cs
+++ MPfm.iOS/Classes/Controllers/EqualizerPresetsViewController.cs
@@ -273,6 +273,8 @@ namespace MPfm.iOS
         public Action<Guid> OnLoadPreset { get; set; }
         public Action<Guid> OnEditPreset { get; set; }
         public Action<Guid> OnDeletePreset { get; set; }
+		public Action<Guid> OnDuplicatePreset { get; set; }
+		public Action<Guid, string> OnExportPreset { get; set; }
 
         public void EqualizerPresetsError(Exception ex)
         {
